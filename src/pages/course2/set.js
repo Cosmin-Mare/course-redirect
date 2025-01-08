4dd5a13fcf14556394378699856eadd2b755bf40
Cosmin-Mare <cosmintudor.mare@cnamd.onmicrosoft.com>
2025-01-08 22:57:57 +0200

    add procfile, etc

--- src/pages/course2/set.js
+++ src/pages/course2/set.js
@@ -1,5 +1,7 @@
 "use client"
 
+import { useEffect } from "react";
+
 export default function Home() {
     function replaceLastCourse(event){
         event.preventDefault();
@@ -9,6 +11,17 @@ export default function Home() {
             body: JSON.stringify({ number: 2, url: event.currentTarget.elements.url.value }),
         });
     }
+
+    useEffect(() => {
+        fetch("/api/get", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ number: 2}),
+        }).then((res1) => 
+            
+            res1.json().then((res) => {console.log(res.url); document.getElementById("url").value = res.url})
+        )
+    }, [])
     return (
         <div>
             <form onSubmit={replaceLastCourse}>
