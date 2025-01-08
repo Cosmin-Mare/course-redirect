ae920f70a3fd1203f209fe6ff0074a49d16af806
Cosmin-Mare <cosmintudor.mare@cnamd.onmicrosoft.com>
2025-01-08 23:12:12 +0200

    vercel debugging

--- src/pages/api/get.js
+++ src/pages/api/get.js
@@ -2,8 +2,11 @@ import fs from "fs";
 
 export default function handler(req, res){
     const body = req.body;
+    const dir = path.resolve('./');
+    const files = fs.readdirSync(dir);
+    console.log({files})
     let text = fs.readFile(`page${body.number}.txt`, 'utf-8', (err, data) => {
+        
         res.status(200).json({url: data})
-        console.log(data)
     })
 }
