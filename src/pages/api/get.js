e6def461f15e2e925e80b777bc523a29f1109b75
Cosmin-Mare <cosmintudor.mare@cnamd.onmicrosoft.com>
2025-01-09 02:04:53 +0200

    modify data read and write

--- src/pages/api/get.js
+++ src/pages/api/get.js
@@ -1,15 +1,9 @@
-import fs from "fs";
-import path from "path";import { put } from "@vercel/blob";
+import { neon } from "@neondatabase/serverless";
 
-export default function handler(req, res){
+export default async function handler(req, res){
     const body = req.body;
-    const { url } = put('articles/blob.txt', 'Hello World!', { access: 'public' });
 
-    const dir = path.resolve('./');
-    const files = fs.readdirSync(dir);
-    console.log({files})
-    let text = fs.readFile(`page${body.number}.txt`, 'utf-8', (err, data) => {
-        
-        res.status(200).json({url: data})
-    })
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    console.log((await sql(`SELECT url FROM pages WHERE num = ${body.number}`))[0].url)
+    res.status(200).json({url: (await sql(`SELECT url FROM pages WHERE num = ${body.number}`))[0].url});
 }
--- src/pages/api/set.js
+++ src/pages/api/set.js
@@ -1,7 +1,11 @@
 import fs from "fs";
+import {put} from "@vercel/blob"
+import { neon } from "@neondatabase/serverless";
 
 export default async function handler(req, res){
     const body = await req.body;
-    await fs.writeFile(`page${body.number}.txt`, body.url, (err, data) => {})
-	res.status(200).json({ message: 'Hello from Next.js!' })
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    console.log(`UPDATE pages SET url = ${body.url} WHERE num = ${body.number}`)
+    await sql(`UPDATE pages SET url = '${body.url}' WHERE num = ${body.number}`)
+	res.status(200).json()
 }
